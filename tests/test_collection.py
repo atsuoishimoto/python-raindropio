584141fec538d2cadcd3190339e411e0fc7adf07
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-06-05 22:39:59 +0900

    update test

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -1,44 +1,86 @@
-from raindropio import api
-
 import datetime
+from unittest.mock import patch
 
+from raindropio import *
+
+collection = {
+ '_id': 1000,
+ 'access': {'draggable': True, 'for': 10000, 'level': 4, 'root': False},
+ 'author': True,
+ 'count': 0,
+ 'cover': [],
+ 'created': '2020-01-01T00:00:00Z',
+ 'creatorRef': {'_id': 10000, 'fullName': 'user name'},
+ 'expanded': False,
+ 'lastUpdate': '2020-01-02T00:00:00Z',
+ 'parent': {'$db': '', '$id': 100, '$ref': 'collections'},
+ 'public': False,
+ 'sort': 3000,
+ 'title': 'child',
+ 'user': {'$db': '', '$id': 10000, '$ref': 'users'},
+ 'view': 'list'
+}
+
+
+
+def test_get_roots():
+    api = API("dummy")
+    with patch('raindropio.api.requests') as m:
+        m.get().json.return_value = {'items': [collection]}
+        for c in Collection.get_roots(api):
+            assert c.id == 1000
+            assert c.access.level == AccessLevel.owner
+            assert c.access.draggable is True
+            assert c.collaborators is None
+            assert c.color is None
+            assert c.count == 0
+            assert c.cover == []
+            assert c.created == datetime.datetime(2020, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
+            assert c.expanded is False
+            assert c.lastUpdate == datetime.datetime(2020, 1, 2, 0, 0, 0, tzinfo=datetime.timezone.utc)
+            assert c.parent.id == 100
+            assert c.public is False
+            assert c.sort == 3000
+            assert c.title == "child"
+            assert c.user.id == 10000
+            assert c.view == View.list
+
+def test_get_childrens():
+    api = API("dummy")
+    with patch('raindropio.api.requests') as m:
+        m.get().json.return_value = {'items': [collection]}
+        for c in Collection.get_childrens(api):
+            assert c.id == 1000
 
 def test_get():
-    for c in api.Collection.get_roots(api.API(token)):
-        print("------------------------------")
-        print("id", c.id)
-        print("access", c.access)
-        print("collaborators", c.collaborators)
-        print("color", c.color)
-        print("count", c.count)
-        print("cover", c.cover)
-        print("created", c.created)
-        print("expanded", c.expanded)
-        print("lastUpdate", c.lastUpdate)
-        print("parent", c.parent)
-        print("public", c.public)
-        print("sort", c.sort)
-        print("title", c.title)
-        print("user", c.user)
-        print("view", c.view)
+    api = API("dummy")
+    with patch('raindropio.api.requests') as m:
+        m.get().json.return_value = {'item': collection}
 
-    for c in api.Collection.get_childrens(api.API(token)):
-        print(c)
-
-    c = api.Collection.get(api.API(token), c.id)
-    print(c)
+        c = Collection.get(api, 1000)
+        assert c.id == 1000
 
 
-def test_put():
-    title = str(datetime.datetime.now())
-    c = api.Collection.update(
-        api.API(token), id=11561930, title=title, view=api.View.list
-    )
-    assert c.title == title
+def test_update():
+    api = API("dummy")
+    with patch('raindropio.api.requests') as m:
+        m.put().json.return_value = {'item': collection}
+        title = str(datetime.datetime.now())
+        c = Collection.update(
+            api, id=1000, title=title, view=View.list
+        )
+        assert c.id == 1000
 
 
-def test_post():
-    c = api.Collection.create(api.API(token), title="abcdef")
-    assert c.title == "abcdef"
+def test_create():
+    api = API("dummy")
+    with patch('raindropio.api.requests') as m:
+        m.post().json.return_value = {'item': collection}
+        c = Collection.create(api, title="abcdef")
+        assert c.id == 1000
 
-    api.Collection.remove(api.API(token), id=c.id)
+def test_delete():
+    api = API("dummy")
+    with patch('raindropio.api.requests') as m:
+        Collection.remove(api, id=1000)
+        assert m.delete.call_args[0] == ('https://api.raindrop.io/rest/v1/collection/1000',)
